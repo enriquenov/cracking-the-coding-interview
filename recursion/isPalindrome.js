b19aeaeb822b6e7b4eaf5804353580e152d325f4
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-23 11:37:51 -0700

    isPalindrome solved recursively.

---
+++ recursion/isPalindrome.js
@@ -0,0 +1,15 @@
+function isPalindrome(str) {
+  if (str.length <= 1) return true;
+
+  if (str.length > 1 && str[0] === str[str.length - 1]) {
+    return isPalindrome(str.slice(1, str.length - 1));
+  }
+
+  return false;
+}
+
+console.log(isPalindrome('awesome')); // false
+console.log(isPalindrome('foobar')); // false
+console.log(isPalindrome('tacocat')); // true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
