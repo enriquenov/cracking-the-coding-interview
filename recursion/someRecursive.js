2844d3c69f56a54eed978b77ec5ffa244480262d
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-23 11:43:14 -0700

    someRecursive solved.

---
+++ recursion/someRecursive.js
@@ -0,0 +1,13 @@
+function someRecursive(arr, func) {
+  if (!arr.length) return false;
+
+  return func(arr[0]) || someRecursive(arr.slice(1), func);
+}
+
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
