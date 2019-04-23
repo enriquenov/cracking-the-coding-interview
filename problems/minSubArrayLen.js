28435bd0ea41a5ec5331a1bf1ab7778624c4914a
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-22 23:13:34 -0700

    minSubArrayLen solved.

---
+++ problems/minSubArrayLen.js
@@ -0,0 +1,23 @@
+function minSubArrayLen(arr, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while (start < arr.length) {
+    if (total < sum && end < arr.length) {
+      total += arr[end];
+      end++;
+    } else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= arr[start];
+      start++;
+    } else {
+      break;
+    }
+  }
+  return minLen === Infinity ? 0 : minLen;
+}
+
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39)); // 3
+console.log(minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 55)); // 5
