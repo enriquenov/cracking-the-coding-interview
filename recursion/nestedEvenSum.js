7e3881bacee2740e5934b9b769367bc16505ab99
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-23 14:16:19 -0700

    nestedEvenSum solved.

---
+++ recursion/nestedEvenSum.js
@@ -0,0 +1,33 @@
+function nestedEvenSum(obj) {
+  let result = 0;
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'number' && !(obj[key] % 2)) result += obj[key];
+    else if (typeof obj[key] === 'object') result += nestedEvenSum(obj[key]);
+  }
+
+  return result;
+}
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: 'yup',
+    },
+  },
+};
+
+var obj2 = {
+  a: 2,
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: 'ball', ccc: 5 },
+  d: 1,
+  e: { e: { e: 2 }, ee: 'car' },
+};
+
+console.log(nestedEvenSum(obj1)); // 6
+console.log(nestedEvenSum(obj2)); // 10
