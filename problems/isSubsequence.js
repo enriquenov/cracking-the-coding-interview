8acea19cd71be8cec87031e2a03bf6ff66d91ae4
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-22 22:27:00 -0700

    isSubsequence solved.

---
+++ problems/isSubsequence.js
@@ -0,0 +1,20 @@
+function isSubsequence(str1, str2) {
+  let idx1 = 0;
+  let idx2 = 0;
+
+  while (idx1 < str1.length && idx2 < str2.length) {
+    if (str1[idx1] === str2[idx2]) {
+      idx1++;
+      idx2++;
+    } else {
+      idx2++;
+    }
+  }
+
+  return idx1 === str1.length;
+}
+
+console.log(isSubsequence('hello', 'hello world'));
+console.log(isSubsequence('sing', 'sting'));
+console.log(isSubsequence('abc', 'abracadabra'));
+console.log(isSubsequence('abc', 'acb'));
