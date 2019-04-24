aaea2e3c33857208d58df9a98d6cee856d35a697
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-23 20:45:28 -0700

    BubbleSort implemented.

---
+++ sorting/bubbleSort.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+  let noSwapped;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    noSwapped = true;
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwapped = false;
+      }
+    }
+    if (noSwapped) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([35, 88, 31, 7, -27, 25, 67, 81, -4]));
--- sorting/selectionSort.js
+++ sorting/selectionSort.js
@@ -1,16 +1,16 @@
 function selectionSort(arr) {
-  let noSwapped;
-  for (let i = arr.length - 1; i >= 0; i--) {
-    noSwapped = true;
-    for (let j = 0; j < i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        noSwapped = false;
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
       }
     }
-    if (noSwapped) break;
+    [arr[i], arr[min]] = [arr[min], arr[i]];
   }
+
   return arr;
 }
 
-console.log(selectionSort([35, 88, 31, 7, -27, 25, 67, 81, -4]));
+console.log(selectionSort([34, 22, 10, 19, 17]));
