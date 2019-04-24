fe16ace623d7f90143db1ba5914e57d6a6566613
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-23 21:59:28 -0700

    QuickSort implemented.

---
+++ sorting/quickSort.js
@@ -0,0 +1,28 @@
+function pivotHelper(arr, start = 0, end = arr.length - 1) {
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      [arr[swapIdx], arr[i]] = [arr[i], arr[swapIdx]];
+    }
+  }
+
+  [arr[swapIdx], arr[start]] = [arr[start], arr[swapIdx]];
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivotHelper(arr, left, right);
+
+    // left
+    quickSort(arr, left, pivotIndex - 1);
+    // right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+console.log(quickSort([34, 22, 10, 19, 17, -32, 10, 25, -3]));
