b89183a65444550b07b08d8836501c0f75118302
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-24 00:21:58 -0700

    Stack implemented.

---
+++ data-structures/stack.js
@@ -0,0 +1,47 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+      this.last = node;
+    } else {
+      let temp = this.first;
+      this.first = node;
+      this.first.next = temp;
+    }
+    return this.size++;
+  }
+
+  pop() {
+    if (this.size === 0) return null;
+    let temp = this.first;
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+let stack = new Stack();
+stack.push(23);
+stack.push(30);
+stack.push(50);
+stack.push(78);
+
+console.log(stack);
