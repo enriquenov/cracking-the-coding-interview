8964bfa0957a3b9e5811955fd97d11fa9760ffa0
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-24 22:39:52 -0700

    Graph: DFT implemented Recursively and Iteratively.

---
+++ data-structures/graphs/graph.js
@@ -0,0 +1,89 @@
+// Undirected Graph
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(v) {
+    if (!this.adjacencyList[v]) {
+      this.adjacencyList[v] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
+  }
+
+  removeVertex(vertex) {
+    this.adjacencyList[vertex].map(v => {
+      this.removeEdge(v, vertex);
+    });
+
+    delete this.adjacencyList[vertex];
+  }
+
+  DFTRecursive(node) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(node);
+
+    return result;
+  }
+
+  DFTIteratively(node) {
+    const stack = [node];
+    const result = [];
+    const visited = {};
+    // const adjacencyList = this.adjacencyList;
+
+    visited[node] = true;
+
+    while (stack.length) {
+      let currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+}
+
+let graph = new Graph();
+graph.addVertex('A');
+graph.addVertex('B');
+graph.addVertex('C');
+graph.addVertex('D');
+graph.addVertex('E');
+graph.addVertex('F');
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('B', 'D');
+graph.addEdge('C', 'E');
+graph.addEdge('D', 'E');
+graph.addEdge('D', 'F');
+graph.addEdge('E', 'F');
+
+console.log(graph);
