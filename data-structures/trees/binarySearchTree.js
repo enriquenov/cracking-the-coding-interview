78cd825ed66d510c56edb0f5639a801c638dfa42
enrique novoa <luisenriquenovoa@gmail.com>
2019-04-24 18:21:36 -0700

    BinarySearchTree implemented.

---
+++ data-structures/trees/binarySearchTree.js
@@ -0,0 +1,71 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  // O(log n) time |
+  insert(val) {
+    let newNode = new Node(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+
+      while (current !== null) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (current.right === null) {
+          current.right = newNode;
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
+  }
+
+  // O(log n) time
+  find(val) {
+    if (this.root === null) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    return found;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(24);
+tree.insert(12);
+tree.insert(45);
+tree.insert(31);
+tree.insert(4);
+tree.insert(6);
+
+console.log(tree);
